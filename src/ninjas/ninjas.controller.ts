99c25f3fb70904f1bd21f272d23e76d48bfa7971
Adedipupo <ajokanola@terragonltd.com>
2023-11-13 13:07:06 +0100

    ninja-controller

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  @Get()
+  getNinjas() {
+    return [];
+  }
+  @Get(':id')
+  getNinja() {
+    return {};
+  }
+  @Post()
+  createNinja() {
+    return {};
+  }
+  @Put(':id')
+  updateNinja() {
+    return {};
+  }
+  @Delete(':id')
+  deleteNinja() {
+    return {};
+  }
+}
